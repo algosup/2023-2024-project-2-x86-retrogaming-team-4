dcadc1ea3b40221d37133cb630f8e395331cc9ff
Maxime CARON <145995231+MaximeAlgosup@users.noreply.github.com>
2023-12-15 10:04:33 +0100

    Start of big manual merge with dev

--- src/interraction.asm
+++ src/interraction.asm
@@ -3,13 +3,20 @@ section .data
     pacManTilePos: dw 0
     pacManCenterX: dw 0
     pacManCenterY: dw 0
+
+    pelletEaten: dw 0
     score: dw 0
     scoreUnit: dd 0
+
     endFrightTime: dd 0, 0
+    
+    endFruitTime: dd 0, 0
+    fruitTilePos: dw 0
+    fruitSprite: dw 0
 
 section .text
 
-    pelletEating:
+     pelletEating:
         mov bx, [strcPacMan + posX]
         add bx, 5
         mov [pacManCenterX], bx
@@ -36,17 +43,43 @@ section .text
         je .pellet    
 
         cmp byte[si], 0x0E
-        je .powerPellet      
+        je .powerPellet
 
+        cmp byte[si], 0x17
+        je .cherry
+
+        cmp byte[si], 0x10
+        je .strawberry
+
+        cmp byte[si], 0x11
+        je .orange
+
+        cmp byte[si], 0x12
+        je .apple
+
+        cmp byte[si], 0x13
+        je .melon
+
+        cmp byte[si], 0x14
+        je .galaxianFlagship
+
+        cmp byte[si], 0x15
+        je .bell
+
+        cmp byte[si], 0x16
+        je .key
+        
         .erase:
-        xor ax, ax
-        xor bx, bx
-        mov ax, [pacManCenterY]
-        mov bx, [pacManCenterX]
-        call replaceTile
-
-
-        ret
+            xor ax, ax
+            xor bx, bx
+            mov ax, [pacManCenterY]
+            mov bx, [pacManCenterX]
+            call replaceTile
+            ret
+        
+        .eraseFruit:
+            call replaceFruit
+            ret
 
         .pellet:
             call isPellet
@@ -55,6 +88,38 @@ section .text
         .powerPellet:
             call isPowerPellet
             jmp .erase
+
+        .cherry:
+            call isCherry
+            jmp .eraseFruit
+
+        .strawberry:
+            call isStrawberry
+            jmp .eraseFruit
+
+        .orange:
+            call isOrange
+            jmp .eraseFruit
+
+        .apple:
+            call isApple
+            jmp .eraseFruit
+
+        .melon:
+            call isMelon
+            jmp .eraseFruit
+
+        .galaxianFlagship:
+            call isGalaxianFlagship
+            jmp .eraseFruit
+
+        .bell:
+            call isBell
+            jmp .eraseFruit
+
+        .key:
+            call isKey
+            jmp .eraseFruit
 
     isPellet:
         ; increment score
@@ -84,6 +149,216 @@ section .text
         xor ax, ax
         xor bx, bx
 
+        ret
+    
+    isCherry:
+        ; increment score
+        add word[score], 200
+        call setTileEmpty
+
+        ret
+
+    isStrawberry:
+        
+        ; increment score
+        add word[score], 300
+        call setTileEmpty
+
+        ret
+
+    isOrange:
+        ; increment score
+        add word[score], 500
+        call setTileEmpty
+
+        ret
+
+    isApple:
+        ; increment score
+        add word[score], 700
+        call setTileEmpty
+
+        ret
+
+    isMelon:
+        ; increment score
+        add word[score], 1000
+        call setTileEmpty
+
+        ret
+
+    isGalaxianFlagship	:
+        ; increment score
+        add word[score], 2000
+        call setTileEmpty
+
+        ret
+
+    isBell:
+        ; increment score
+        add word[score], 3000
+        call setTileEmpty
+
+        ret
+
+    isKey:
+        ; increment score
+        add word[score], 5000
+        call setTileEmpty
+
+        ret
+
+    addCherry:
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+
+        cmp byte[si], 0x0F
+        jne .exit
+
+        mov byte[si], 0x17
+        mov word[fruitSprite], CHERRY
+        call displayFruit
+
+        mov eax, [timestamp_of_next_frame]
+        add eax, 30000000
+        mov [endFruitTime], eax
+
+        .exit:
+        ret
+
+    addStrawberry:
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+
+        cmp byte[si], 0x0F
+        jne .exit
+
+        mov byte[si], 0x10
+        mov word[fruitSprite], STRAWBERRY
+        call displayFruit
+
+        mov eax, [timestamp_of_next_frame]
+        add eax, 30000000
+        mov [endFruitTime], eax
+
+        .exit:
+        ret
+
+    addOrange:
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+
+        cmp byte[si], 0x0F
+        jne .exit
+
+        mov byte[si], 0x11
+        mov word[fruitSprite], ORANGE
+        call displayFruit
+
+        mov eax, [timestamp_of_next_frame]
+        add eax, 30000000
+        mov [endFruitTime], eax
+        
+        .exit:
+        ret
+
+    addApple:
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+
+        cmp byte[si], 0x0F
+        jne .exit
+
+        mov byte[si], 0x12
+        mov word[fruitSprite], APPLE
+        call displayFruit
+
+        mov eax, [timestamp_of_next_frame]
+        add eax, 30000000
+        mov [endFruitTime], eax
+        
+        .exit:
+        ret
+
+    addMelon:
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+
+        cmp byte[si], 0x0F
+        jne .exit
+
+        mov byte[si], 0x13
+        mov word[fruitSprite], MELON
+        call displayFruit
+
+        mov eax, [timestamp_of_next_frame]
+        add eax, 30000000
+        mov [endFruitTime], eax
+        
+        .exit:
+        ret
+
+    addGalaxianFlagship:
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+
+        cmp byte[si], 0x0F
+        jne .exit
+
+        mov byte[si], 0x14
+        mov word[fruitSprite], GALAXIAN
+        call displayFruit
+
+        mov eax, [timestamp_of_next_frame]
+        add eax, 30000000
+        mov [endFruitTime], eax
+        
+        .exit:
+        ret
+
+    addBell:
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+
+        cmp byte[si], 0x0F
+        jne .exit
+
+        mov byte[si], 0x15
+        mov word[fruitSprite], BELL
+        call displayFruit
+
+        mov eax, [timestamp_of_next_frame]
+        add eax, 30000000
+        mov [endFruitTime], eax
+        
+        .exit:
+        ret
+    
+    addKey:
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+
+        cmp byte[si], 0x0F
+        jne .exit
+
+        mov byte[si], 0x16
+        mov word[fruitSprite], KEY
+        call displayFruit
+
+        mov eax, [timestamp_of_next_frame]
+        add eax, 30000000
+        mov [endFruitTime], eax
+        
+        .exit:
+        ret
+
+    setTileEmpty:
+        ; get the tile at pacman's position
+        mov si, MazeModelBuffer
+        add si, [pacManTilePos]
+
+        ; set the tile to empty
+        mov byte[si], 0x0F
         ret
 
     frightTime:
@@ -128,4 +403,45 @@ section .text
         jne .noTP
         sub word[strcPacMan + posX], 303
         .noTP:
+        ret
+
+    setFruits:
+        cmp word[pelletEaten], 70
+        je addCherry
+        
+        cmp word[pelletEaten], 170
+        je addStrawberry
+
+        cmp word[pelletEaten], 270
+        je addOrange
+
+        cmp word[pelletEaten], 370
+        je addApple
+
+        cmp word[pelletEaten], 470
+        je addMelon
+
+        cmp word[pelletEaten], 570
+        je addGalaxianFlagship
+
+        cmp word[pelletEaten], 670
+        je addBell
+
+        cmp word[pelletEaten], 770
+        je addKey
+
+        ret
+
+    checkFruitPrint:
+        mov eax, [timestamp_of_next_frame]
+        cmp eax, [endFruitTime]
+        jne .stillFruitTime
+
+        int3
+        mov si, MazeModelBuffer
+        add si, FRUIT_TILE_POS
+        mov byte[si], 0x0F
+        call replaceFruit
+
+        .stillFruitTime:
         ret
--- src/sprites.asm
+++ src/sprites.asm
@@ -100,6 +100,17 @@ section .text
         call draw_sprite
 
         ret
+    
+    displayFruit:
+    ; display the fruit according to its current position values
+        mov ax, [fruitSprite]
+        call calculate_spritesheet_position
+        mov dx, FRUIT_POS_X + FRUIT_POS_Y
+        push dx
+        call draw_sprite
+        pop dx
+        call draw_sprite_bg_buffer
+        ret
 
     displayScore:
         mov eax, [score]
@@ -112,23 +123,23 @@ section .text
 
         ret
 
-    .scoreLoop:
-        inc long [currentDisplayedScoreUnit] ; Add one to the unit we edit
+        .scoreLoop:
+            inc long [currentDisplayedScoreUnit] ; Add one to the unit we edit
 
-        cmp long [currentDisplayedScoreUnit], 8 ; If we are at the eight number, we break
-        je .zero
+            cmp long [currentDisplayedScoreUnit], 8 ; If we are at the eight number, we break
+            je .zero
 
-    .notZeroDisplayScore:
-        xor edx, edx
-        int3
-        mov cx, 10 
-        push eax          ; As eax contains the score, we need to preserve it
-        div cx ;Divides the score => 
-        mov [scoreUnit], edx ; reminder is stored in dx
-        call displayUnit
-        pop eax
-        call .scoreLoop
-        ret
+        .notZeroDisplayScore:
+            xor edx, edx
+            int3
+            mov cx, 10 
+            push eax          ; As eax contains the score, we need to preserve it
+            div cx ;Divides the score => 
+            mov [scoreUnit], edx ; reminder is stored in dx
+            call displayUnit
+            pop eax
+            call .scoreLoop
+            ret
 
 
     displayUnit: ;ax usable to store data
@@ -342,6 +353,52 @@ section .text
                 je .writeBgBuffer
 
         ret
+    
+    replaceFruit:
+        xor ax, ax
+        xor bx, bx
+        ; replace the 16x16 bloc of pixels where is tile, by the content of the background buffer at the same location, according to its x y positions
+        ; calculate the linear position of the tile
+
+        .updateThePixeledBuffers:
+        ; overwrite the pellet with background color into the screenbuffer and the backgroundbuffer
+            mov bx, FRUIT_POS_X + FRUIT_POS_Y
+
+            ;set the destination 'es:di'
+            push word [ScreenBufferSegment] 
+            pop es 
+            mov di, bx
+            mov ah, 1
+
+            ;set the source 'al' : the background color
+            mov al, BACKGROUND_COLOR
+            mov dx, SPRITE_SIZE
+
+            jmp .writeTheTile
+
+            .writeBgBuffer:
+            ;set the destination 'es:di'
+            push word [BackgroundBufferSegment] 
+            pop es 
+            mov di, bx
+            mov ah, 0
+
+            
+            .writeTheTile:
+                ;set the source 'al' : the background color
+                mov al, BACKGROUND_COLOR
+                
+                mov dx, SPRITE_SIZE
+                .eachLine:
+                    mov cx, SPRITE_SIZE
+                    rep stosb
+                    add di, SCREEN_WIDTH - SPRITE_SIZE
+                    dec dx
+                    jnz .eachLine
+                cmp ah, 1
+                je .writeBgBuffer
+
+            ret
 
 
     calculate_screen_position:
@@ -437,14 +494,77 @@ section .text
         ret
 
     draw_tile:
-    mov ax, [ScreenBufferSegment]
-    mov es, ax
-    mov dx, TILE_SIZE ;number of lines
-    .eachLine:
-        mov cx, TILE_SIZE
-        rep movsb
-        add di, SCREEN_WIDTH - TILE_SIZE
-        dec dx
-        cmp dx, 0
+        mov ax, [ScreenBufferSegment]
+        mov es, ax
+        mov dx, TILE_SIZE ;number of lines
+        .eachLine:
+            mov cx, TILE_SIZE
+            rep movsb
+            add di, SCREEN_WIDTH - TILE_SIZE
+            dec dx
+            cmp dx, 0
             jne .eachLine
+        
+        ret
+
+    draw_sprite_bg_buffer:
+    ; this function needs : 
+    ; bx: Base position on spritesheet
+    ; dx: Linear position on screen to display
+
+        push es ; pop at the end of the function
+
+        ; Decrement as increment is at the beginning of the loop
+        dec dx
+        ; Number of pixels draw
+        mov cx, 0x00ff ; TODO: DSP
+
+        .draw_loop:
+
+            inc dx
+            inc cl ; TODO: DSP
+
+            ; Read the color index
+            push cx
+            shr cx, 1 ; Because there are two pixels in a byte
+            mov di, spritesheet
+            add di, bx
+            add di, cx
+            mov al, [ds:di]
+
+            ; Check if the color is in the high or low nibble
+            pop cx
+            test cl, 1 ; TODO: DSP
+            jnz .low_byte
+            shr ax, 4
+
+            .low_byte:
+            and ax, 0xf ; TODO: DSP
+            je .transparent_skip
+
+            ; Get color from palette
+            mov di, palette
+            add di, ax
+            mov al, [ds:di]
+
+            ; Set the pixel
+            mov di, dx
+            
+            push word [BackgroundBufferSegment] 
+            pop es
+            mov [es:di], al
+            
+
+            ; If we need to go to the next line
+            .transparent_skip:
+                not cl
+                test cl, 0xf ; TODO: DSP
+                not cl
+                jne .draw_loop
+                add dx, SCREEN_WIDTH - SPRITE_SIZE
+                cmp cl, 0xff
+                jne .draw_loop
+
+        pop es
+
         ret
