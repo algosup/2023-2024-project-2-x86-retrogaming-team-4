cbd34a23e1eee60562d8f5c4e51fb06b66cb91a3
Benoît de Keyn <146000855+benoitdekeyn-algosup@users.noreply.github.com>
2023-12-15 19:30:29 +0100

    the speed of pacman is 1.5 pixels/frame

--- src/keyboard.asm
+++ src/keyboard.asm
@@ -2,8 +2,108 @@ section .data
 
     keyPressed dw 0
     keyChanged db 0
+    jumpNoLeft db 0
+    jumpNoRight db 0
+    jumpNoUp db 0
+    jumpNoDown db 0
 
 section .text
+    checkLeft:
+        call getCorners
+        call getTileAbsPos
+        mov si, MazeModel
+        add si, [tileAbsPos]
+        sub si, 1
+        cmp byte[si], 0x0B
+        je .NoLeft
+        cmp byte[si], 0x06
+        je .NoLeft
+        cmp byte[si], 0x08
+        je .NoLeft
+        cmp byte[si], 0x02
+        je .NoLeft
+        cmp byte[si], 0x04
+        je .NoLeft
+
+        .end:
+            ret
+
+        .NoLeft:
+            mov byte[jumpNoLeft], 1
+            jmp .end
+
+    checkRight:
+        call getCorners
+        call getTileAbsPos
+        mov si, MazeModel
+        add si, [tileAbsPos]
+        add si, 1
+        cmp byte[si], 0x0C
+        je .NoRight
+        cmp byte[si], 0x07
+        je .NoRight
+        cmp byte[si], 0x05
+        je .NoRight
+        cmp byte[si], 0x03
+        je .NoRight
+        cmp byte[si], 0x01
+        je .NoRight
+
+        .end:
+            ret
+
+        .NoRight:
+            mov byte[jumpNoRight], 1
+            jmp .end
+
+
+    checkUp:
+        call getCorners
+        call getTileAbsPos
+        mov si, MazeModel
+        add si, [tileAbsPos]
+        sub si, 40
+        cmp byte[si], 0x09
+        je .NoUp
+        cmp byte[si], 0x07
+        je .NoUp
+        cmp byte[si], 0x08
+        je .NoUp
+        cmp byte[si], 0x03
+        je .NoUp
+        cmp byte[si], 0x04
+        je .NoUp
+
+        .end:
+            ret
+
+        .NoUp:
+            mov byte[jumpNoUp], 1
+            jmp .end
+
+    checkDown:
+        call getCorners
+        call getTileAbsPos
+        mov si, MazeModel
+        add si, [tileAbsPos]
+        add si, 40
+        cmp byte[si], 0x0A
+        je .NoDown
+        cmp byte[si], 0x06
+        je .NoDown
+        cmp byte[si], 0x05
+        je .NoDown
+        cmp byte[si], 0x01
+        je .NoDown
+        cmp byte[si], 0x02
+        je .NoDown
+
+        .end:
+            ret
+
+        .NoDown:
+            mov byte[jumpNoDown], 1
+            jmp .end
 
     readKeyboard:
     
@@ -34,87 +134,32 @@ section .text
         cmp byte [keyPressed], LEFT_KEY_SCANCODE
         jne .NoLeft
 
-        ; We use the same kind of process as used for collisions, but we only check walls were we cannot go left
-        ; The direction stays in buffer until Pac-Man can go left
-
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 4
         mov word [cornerOffsetY], 4
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        sub si, 1
-        cmp byte[si], 0x0B
+        call checkLeft
+        cmp byte[jumpNoLeft], 1
         je .NoLeft
-        cmp byte[si], 0x06
-        je .NoLeft
-        cmp byte[si], 0x08
-        je .NoLeft
-        cmp byte[si], 0x02
-        je .NoLeft
-        cmp byte[si], 0x04
-        je .NoLeft
+        
 
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 11
         mov word [cornerOffsetY], 4
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        sub si, 1
-        cmp byte[si], 0x0B
-        je .NoLeft
-        cmp byte[si], 0x06
-        je .NoLeft
-        cmp byte[si], 0x08
-        je .NoLeft
-        cmp byte[si], 0x02
-        je .NoLeft
-        cmp byte[si], 0x04
+        call checkLeft
+        cmp byte[jumpNoLeft], 1
         je .NoLeft
 
 
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 4
         mov word [cornerOffsetY], 11
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        sub si, 1
-        cmp byte[si], 0x0B
-        je .NoLeft
-        cmp byte[si], 0x06
-        je .NoLeft
-        cmp byte[si], 0x08
-        je .NoLeft
-        cmp byte[si], 0x02
-        je .NoLeft
-        cmp byte[si], 0x04
+        call checkLeft
+        cmp byte[jumpNoLeft], 1
         je .NoLeft
 
 
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 11
         mov word [cornerOffsetY], 11
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        sub si, 1
-        cmp byte[si], 0x0B
+        call checkLeft
+        cmp byte[jumpNoLeft], 1
         je .NoLeft
-        cmp byte[si], 0x06
-        je .NoLeft
-        cmp byte[si], 0x08
-        je .NoLeft
-        cmp byte[si], 0x02
-        je .NoLeft
-        cmp byte[si], 0x04
-        je .NoLeft
-
 
         mov word[strcPacMan + velocityX], -1
         mov word[strcPacMan + velocityY], 0
@@ -122,88 +167,35 @@ section .text
         call changePacManPosition
         inc byte[keyChanged]
         .NoLeft:
+        mov byte[jumpNoLeft], 0
 
         ; Right
         cmp byte [keyPressed], RIGHT_KEY_SCANCODE
         jne .NoRight
 
-        ; We use the same kind of process as used for collisions, but we only check walls were we cannot go right
-        ; The direction stays in buffer until Pac-Man can go right
-
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 4
         mov word [cornerOffsetY], 4
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        add si, 1
-        cmp byte[si], 0x0C
-        je .NoRight
-        cmp byte[si], 0x07
-        je .NoRight
-        cmp byte[si], 0x05
-        je .NoRight
-        cmp byte[si], 0x03
-        je .NoRight
-        cmp byte[si], 0x01
+        call checkRight
+        cmp byte[jumpNoRight], 1
         je .NoRight
 
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 11
         mov word [cornerOffsetY], 4
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        add si, 1
-        cmp byte[si], 0x0C
-        je .NoRight
-        cmp byte[si], 0x07
-        je .NoRight
-        cmp byte[si], 0x05
-        je .NoRight
-        cmp byte[si], 0x03
-        je .NoRight
-        cmp byte[si], 0x01
+        call checkRight
+        cmp byte[jumpNoRight], 1
         je .NoRight
 
-        ; Set offset for the corner to check
+
         mov word [cornerOffsetX], 4
         mov word [cornerOffsetY], 11
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        add si, 1
-        cmp byte[si], 0x0C
-        je .NoRight
-        cmp byte[si], 0x07
-        je .NoRight
-        cmp byte[si], 0x05
-        je .NoRight
-        cmp byte[si], 0x03
-        je .NoRight
-        cmp byte[si], 0x01
+        call checkRight
+        cmp byte[jumpNoRight], 1
         je .NoRight
 
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 11
         mov word [cornerOffsetY], 11
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        add si, 1
-        cmp byte[si], 0x0C
-        je .NoRight
-        cmp byte[si], 0x07
-        je .NoRight
-        cmp byte[si], 0x05
-        je .NoRight
-        cmp byte[si], 0x03
-        je .NoRight
-        cmp byte[si], 0x01
+        call checkRight
+        cmp byte[jumpNoRight], 1
         je .NoRight
 
         mov word[strcPacMan + velocityX], 1
@@ -212,88 +204,34 @@ section .text
         call changePacManPosition
         inc byte[keyChanged]
         .NoRight:
+        mov byte[jumpNoRight], 0
 
         ; Up
         cmp byte [keyPressed], UP_KEY_SCANCODE
         jne .NoUp
 
-        ; We use the same kind of process as used for collisions, but we only check walls were we cannot go up
-        ; The direction stays in buffer until Pac-Man can go up
-        
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 4
         mov word [cornerOffsetY], 4
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        sub si, 40
-        cmp byte[si], 0x09
-        je .NoUp
-        cmp byte[si], 0x07
-        je .NoUp
-        cmp byte[si], 0x08
-        je .NoUp
-        cmp byte[si], 0x03
-        je .NoUp
-        cmp byte[si], 0x04
+        call checkUp
+        cmp byte[jumpNoUp], 1
         je .NoUp
         
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 11
         mov word [cornerOffsetY], 4
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        sub si, 40
-        cmp byte[si], 0x09
+        call checkUp
+        cmp byte[jumpNoUp], 1
         je .NoUp
-        cmp byte[si], 0x07
-        je .NoUp
-        cmp byte[si], 0x08
-        je .NoUp
-        cmp byte[si], 0x03
-        je .NoUp
-        cmp byte[si], 0x04
-        je .NoUp
-        
-        ; Set offset for the corner to check
+
         mov word [cornerOffsetX], 4
         mov word [cornerOffsetY], 11
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        sub si, 40
-        cmp byte[si], 0x09
-        je .NoUp
-        cmp byte[si], 0x07
-        je .NoUp
-        cmp byte[si], 0x08
-        je .NoUp
-        cmp byte[si], 0x03
-        je .NoUp
-        cmp byte[si], 0x04
+        call checkUp
+        cmp byte[jumpNoUp], 1
         je .NoUp
         
-        ; Set offset for the corner to check
         mov word [cornerOffsetX], 11
         mov word [cornerOffsetY], 11
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        sub si, 40
-        cmp byte[si], 0x09
-        je .NoUp
-        cmp byte[si], 0x07
-        je .NoUp
-        cmp byte[si], 0x08
-        je .NoUp
-        cmp byte[si], 0x03
-        je .NoUp
-        cmp byte[si], 0x04
+        call checkUp
+        cmp byte[jumpNoUp], 1
         je .NoUp
 
         mov word[strcPacMan + velocityX], 0
@@ -302,97 +240,45 @@ section .text
         call changePacManPosition
         inc byte[keyChanged]
         .NoUp:
+        mov byte[jumpNoUp], 0
 
         ; Down
         cmp byte [keyPressed], DOWN_KEY_SCANCODE
         jne .NoDown
 
 
-        ; We use the same kind of process as used for collisions, but we only check walls were we cannot go down
-        ; The direction stays in buffer until Pac-Man can go down
+        mov word [cornerOffsetX], 4
+        mov word [cornerOffsetY], 4
+        call checkDown
+        cmp byte[jumpNoDown], 1
+        je .NoDown
+        
+        mov word [cornerOffsetX], 11
+        mov word [cornerOffsetY], 4
+        call checkDown
+        cmp byte[jumpNoDown], 1
+        je .NoDown
+        
+        mov word [cornerOffsetX], 4
+        mov word [cornerOffsetY], 11
+        call checkDown
+        cmp byte[jumpNoDown], 1
+        je .NoDown
+        
+        mov word [cornerOffsetX], 11
+        mov word [cornerOffsetY], 11
+        call checkDown
+        cmp byte[jumpNoDown], 1
+        je .NoDown
 
-        ; Set offset for the corner to check
-        mov word [cornerOffsetX], 4
-        mov word [cornerOffsetY], 4
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        add si, 40
-        cmp byte[si], 0x0A
-        je .NoDown
-        cmp byte[si], 0x06
-        je .NoDown
-        cmp byte[si], 0x05
-        je .NoDown
-        cmp byte[si], 0x01
-        je .NoDown
-        cmp byte[si], 0x02
-        je .NoDown
-        
-        ; Set offset for the corner to check
-        mov word [cornerOffsetX], 11
-        mov word [cornerOffsetY], 4
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        add si, 40
-        cmp byte[si], 0x0A
-        je .NoDown
-        cmp byte[si], 0x06
-        je .NoDown
-        cmp byte[si], 0x05
-        je .NoDown
-        cmp byte[si], 0x01
-        je .NoDown
-        cmp byte[si], 0x02
-        je .NoDown
-        
-        ; Set offset for the corner to check
-        mov word [cornerOffsetX], 4
-        mov word [cornerOffsetY], 11
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        add si, 40
-        cmp byte[si], 0x0A
-        je .NoDown
-        cmp byte[si], 0x06
-        je .NoDown
-        cmp byte[si], 0x05
-        je .NoDown
-        cmp byte[si], 0x01
-        je .NoDown
-        cmp byte[si], 0x02
-        je .NoDown
-        
-        ; Set offset for the corner to check
-        mov word [cornerOffsetX], 11
-        mov word [cornerOffsetY], 11
-        call getCorners
-        call getTileAbsPos
-        mov si, MazeModel
-        add si, [tileAbsPos]
-        add si, 40
-        cmp byte[si], 0x0A
-        je .NoDown
-        cmp byte[si], 0x06
-        je .NoDown
-        cmp byte[si], 0x05
-        je .NoDown
-        cmp byte[si], 0x01
-        je .NoDown
-        cmp byte[si], 0x02
-        je .NoDown
-        
+
         mov word[strcPacMan + velocityX], 0
         mov word[strcPacMan + velocityY], 1
         mov word[strcPacMan + direction], DOWN_DIRECTION
         call changePacManPosition
         inc byte[keyChanged]
         .NoDown:
+            mov byte[jumpNoDown], 0
             cmp byte [keyChanged], 0
             jne .NoChange
             call changePacManPosition
